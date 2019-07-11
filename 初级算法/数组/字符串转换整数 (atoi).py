75801bebd1951ad16363f9e178473b25483e0bd4
Alignhao <yanghaodong@hrbeu.edu.cn>
2019-07-11 21:07:47 +0800

    Update 字符串转换整数 (atoi).py

--- 初级算法/数组/字符串转换整数 (atoi).py
+++ 初级算法/数组/字符串转换整数 (atoi).py
@@ -48,5 +48,5 @@
 # method1   正则表达式---------------------------------------------
 class Solution:
     def myAtoi(self, s: str) -> int:
-        return max(min(int(*re.findall('^[\+\-]?\d+', s.lstrip())), 2**31 - 1), -2**31)
+        return max(min(int(*re.findall('^[\+\-]?\d+', str.lstrip())), 2**31 - 1), -2**31)
 
