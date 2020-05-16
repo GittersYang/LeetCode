f83ef73f4e9837a32a963e26483540c94afa5fef
Alignhao <yanghaodong@hrbeu.edu.cn>
2020-05-16 08:40:57 +0800

    Update 从排序数组中删除重复项.py

--- 初级算法/数组/从排序数组中删除重复项.py
+++ 初级算法/数组/从排序数组中删除重复项.py
@@ -2,8 +2,26 @@
 示例 1:给定数组 nums = [1,1,2], 
 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。 
 
-方法一
+方法一 cpp
 ----------------------------------------------------------
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        int j=1;
+        if(nums.size()<=1) return nums.size();
+        for(;j<nums.size();j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+        }
+    
+};
+方法二
+----------------------------------------------------
 """
 i=0
 while i<len(nums)-1:
@@ -13,7 +31,7 @@ while i<len(nums)-1:
         i=i+1
 return len(nums)
 """
-方法二 优秀
+方法三 优秀
 -------------------------------------------------------
 “”“
 class Solution:
